d24abcb015547ea6c9ff25634dfe6981fefa1b00
Smokewolf <smokewolfy@gmail.com>
2024-01-27 07:19:42 +0300

    Initial Commit

---
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -0,0 +1,13 @@
+package lesson2
+
+fun main(){
+    val departureHour = 9
+    val departureMinute = 39
+
+    val travelTime = 457
+
+    val arrivalMinute = (departureMinute + travelTime) % 60
+    val arrivalHour = departureHour + (departureMinute + travelTime) / 60
+
+    println("Время прибытия - ${String.format("%02d:%02d", arrivalHour, arrivalMinute)}")
+}
