1b0b23e94890c53b5de7b0ce1334e4d54f1994a2
Smokewolf <smokewolfy@gmail.com>
2024-01-27 18:53:21 +0300

    Kicked out magick numbers

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -4,11 +4,11 @@ fun main(){
 
     val departureHour = 9
     val departureMinute = 39
-
+    val minutesInHour = 60
     val travelTime = 457
 
-    val arrivalMinute = (departureMinute + travelTime) % 60
-    val arrivalHour = departureHour + (departureMinute + travelTime) / 60
+    val arrivalMinute = (departureMinute + travelTime) % minutesInHour
+    val arrivalHour = departureHour + (departureMinute + travelTime) / minutesInHour
 
     println("Время прибытия - ${String.format("%02d:%02d", arrivalHour, arrivalMinute)}")
 }
