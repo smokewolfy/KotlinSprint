8f9996281796da68190309e615ffc96a4e6290d5
Smokewolf <smokewolfy@gmail.com>
2024-01-28 11:41:29 +0300

    Kicked out magick numbers

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -1,12 +1,13 @@
 package lesson2
 
 fun main(){
-    val baff = 0.2f
+    val baffPercentage = 20
     val crystalOre = 7
     val ironOre = 11
+    val devider = 100
 
-    val baffCrystalOre: Int = (crystalOre * baff).toInt()
-    val baffIronOre: Int = (ironOre * baff).toInt()
+    val baffCrystalOre: Int = (crystalOre * baffPercentage)/devider
+    val baffIronOre: Int = (ironOre * baffPercentage)/devider
 
     println("Получено дополнительно кристаллической руды - $baffCrystalOre")
     println("Получено дополнительно железной руды - $baffIronOre")
