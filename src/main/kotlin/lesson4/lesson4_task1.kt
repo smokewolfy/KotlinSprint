90ca1ceb8039bc4febc70858c7f7a95fec0f41c6
Smokewolf <smokewolfy@gmail.com>
2024-02-01 19:08:28 +0300

    KS-4-1

--- src/main/kotlin/lesson4/lesson4_task1.kt
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -1,9 +1,9 @@
 package lesson4
-
+const val allTableCount = 13
 fun main() {
-    val allTableCount = 13
-    var reservedTableForToday = 13
-    var reservedTableForTomorrow = 9
+
+    val reservedTableForToday = 13
+    val reservedTableForTomorrow = 9
     println("Доступность столиков на сегодня: ${allTableCount > reservedTableForToday}\n" +
             "Доступность столиков на завтра: ${allTableCount > reservedTableForTomorrow}")
     println()
