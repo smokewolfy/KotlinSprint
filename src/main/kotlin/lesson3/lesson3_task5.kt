e9d2223aeea3453c6228dadea9d7311c7b15e674
Smokewolf <smokewolfy@gmail.com>
2024-02-01 19:12:38 +0300

    KS-3-5

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -1,14 +1,11 @@
 package lesson3
 
 fun main() {
-    var input = "D2-D4;0"
-    var moveFrom = input.substringBefore("-")
-    var moveTo = input.substringAfter("-").substringBefore(";")
-    var moveCount = input.substringAfter(";").toInt()
-    println("""
-        |Ход номер $moveCount
-        |Белые ходят:
-        |С $moveFrom на $moveTo
-        |
-    """.trimMargin())
+    val input = "D2-D4;0"
+    val moveFrom = input.substringBefore("-")
+    val moveTo = input.substringAfter("-").substringBefore(";")
+    val moveCount = input.substringAfter(";").toInt()
+    println(moveFrom)
+    println(moveTo)
+    println(moveCount)
 }
