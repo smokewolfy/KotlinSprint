425ddce23f60dc43ab250c758e79cf82eb071711
Smokewolf <smokewolfy@gmail.com>
2024-02-02 08:38:46 +0300

    Исправил имя константы

--- src/main/kotlin/lesson4/lesson4_task1.kt
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -1,11 +1,11 @@
 package lesson4
-const val allTableCount = 13
+const val ALL_TABLE_COUNT = 13
 fun main() {
 
     val reservedTableForToday = 13
     val reservedTableForTomorrow = 9
-    println("Доступность столиков на сегодня: ${allTableCount > reservedTableForToday}\n" +
-            "Доступность столиков на завтра: ${allTableCount > reservedTableForTomorrow}")
+    println("Доступность столиков на сегодня: ${ALL_TABLE_COUNT > reservedTableForToday}\n" +
+            "Доступность столиков на завтра: ${ALL_TABLE_COUNT > reservedTableForTomorrow}")
     println()
 
 }
