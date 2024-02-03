b29a9b5072a005dc5508252fe2d1b1ab8ff63af5
Smokewolf <smokewolfy@gmail.com>
2024-02-03 21:27:53 +0300

    KS-5-2

--- src/main/kotlin/lesson5/lesson5_task2.kt
+++ src/main/kotlin/lesson5/lesson5_task2.kt
@@ -6,12 +6,14 @@ fun main() {
     val currentYear = 2024
     val userAge = currentYear - userBirthYear
 
-    val resultText = if (userAge >= AGE_OF_MAJORITY) "Показать экран со скрытым контентом"
-    else if (userAge == 16 || userAge == 17) "Показать экран с ограниченным контентом"
-    else "Вернуться на главный экран"
-
+    val resultText = when {
+        userAge >= AGE_OF_MAJORITY -> "Показать экран со скрытым контентом"
+        userAge in FIRST_AGE_OF_PRE_MAJORITY..SECOND_AGE_OF_PRE_MAJORITY -> "Показать экран с ограниченным контентом"
+        else -> "Вернуться на главный экран"
+    }
     println(resultText)
 }
 
-
+const val FIRST_AGE_OF_PRE_MAJORITY = 16
+const val SECOND_AGE_OF_PRE_MAJORITY = 17
 const val AGE_OF_MAJORITY = 18
