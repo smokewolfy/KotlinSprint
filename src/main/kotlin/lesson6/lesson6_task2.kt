8a43bb36c3d32519419bb66dba85249d94a4aba8
Smokewolf <smokewolfy@gmail.com>
2024-02-07 11:00:25 +0300

    KS-6-2

--- src/main/kotlin/lesson6/lesson6_task2.kt
+++ src/main/kotlin/lesson6/lesson6_task2.kt
@@ -2,11 +2,7 @@ package lesson6
 
 fun main() {
     print("Введите количество секунд: ")
-    val inputSec = readln().toInt()
-    var timer = inputSec
-    while (timer > 0){
-        println("Осталось ${timer--}")
-        Thread.sleep(1000)
-    }
+    val inputSec = readln().toLong()
+    Thread.sleep(inputSec * 1000)
     println("Прошло $inputSec секунд")
 }
