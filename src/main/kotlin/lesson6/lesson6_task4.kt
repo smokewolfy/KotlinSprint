3596617b626e15c13a9b5f11ebd489f43992a92d
Smokewolf <smokewolfy@gmail.com>
2024-02-09 07:17:33 +0300

    KS-6-4

--- src/main/kotlin/lesson6/lesson6_task4.kt
+++ src/main/kotlin/lesson6/lesson6_task4.kt
@@ -2,19 +2,18 @@ package lesson6
 
 fun main() {
     var tryCount = 5
-    var didNotWin = true
     val number = (1..9).random()
-    while (tryCount > 0 && didNotWin) {
+    while (tryCount > 0) {
         --tryCount
         print("Попытка ${5 - tryCount}. Введите число: ")
         if (readln().toInt() == number) {
-            didNotWin = false
             println("Это была великолепная игра!")
+            break
         }else{
             println("Неверно.")
         }
     }
-    if (didNotWin && tryCount == 0) {
+    if (tryCount == 0) {
         println("Было загадано число $number")
     }
 }
