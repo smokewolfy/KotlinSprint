649196ef0debcc3be213ce39acc11bc1c3af89a2
Smokewolf <smokewolf@yandex.ru>
2024-02-16 20:32:36 +0300

    KS-8-5

--- src/main/kotlin/lesson8/lesson8_task5.kt
+++ src/main/kotlin/lesson8/lesson8_task5.kt
@@ -3,9 +3,9 @@ package lesson8
 fun main() {
     print("Введите количество элементов: ")
     val array = Array<String>(readln().toInt()) { "" }
-    for (i in array) {
-        print("Введите элемент №${array.indexOf(i) + 1}: ")
-        array.set(array.indexOf(i), readln())
+    for (i in array.indices) {
+        print("Введите элемент №${i + 1}: ")
+        array.set(i, readln())
     }
     println("Ваш массив: " + array.joinToString())
 }
