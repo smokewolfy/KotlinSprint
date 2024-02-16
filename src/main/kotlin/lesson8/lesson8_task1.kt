68e2b246985010b14f326c887b897b43fbff16d5
Smokewolf <smokewolf@yandex.ru>
2024-02-16 21:28:53 +0300

    KS-8-1

--- src/main/kotlin/lesson8/lesson8_task1.kt
+++ src/main/kotlin/lesson8/lesson8_task1.kt
@@ -2,9 +2,7 @@ package lesson8
 
 fun main() {
     val weekViewsPerDay = intArrayOf(305, 800, 1000, 608, 762, 901, 506)
-    var allWeekViews = 0
-    for (i in weekViewsPerDay) {
-        allWeekViews += i
-    }
+    val allWeekViews = weekViewsPerDay.sum()
+
     println("Ваши просмотры за день: $allWeekViews")
 }
