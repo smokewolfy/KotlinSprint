1990fe370423a7e99ba0ae808bca779aebaf7efe
Smokewolf <smokewolf@yandex.ru>
2024-02-28 12:05:27 +0300

    KS-11-4

--- src/main/kotlin/lesson11/lesson11_task4.kt
+++ src/main/kotlin/lesson11/lesson11_task4.kt
@@ -1,32 +1,15 @@
 package lesson11
 
 
-class CategoryList(
-    private val categoryList: MutableList<Category> = mutableListOf(
-        Category(
-            categoryShortDescription = "",
-            categoryName = "Favorite",
-            recipeList = mutableListOf(),
-        )
-    ),
-
-    ) {
-    fun addCategory(categoryShortDescription: String, categoryName: String) {
-        categoryList.add(Category(categoryShortDescription, categoryName))
-    }
-}
-
-
 class Category(
     private val categoryShortDescription: String,
     private val categoryName: String,
-    val recipeList: MutableList<Recipe> = mutableListOf(),
+    internal val recipeList: MutableList<Recipe> = mutableListOf(),
 ) {
-    fun addRecipe(recipeName: String,recipeDescription: String) {
+    fun addRecipe(recipeName: String, recipeDescription: String) {
         recipeList.add(
             Recipe(
-                recipeName = recipeName ,
-                recipeDescription = recipeDescription
+                recipeName = recipeName, recipeDescription = recipeDescription
             )
         )
     }
@@ -38,15 +21,6 @@ class Recipe(
     private val recipeDescription: String,
 
     ) {
-    ////////
-    class Ingredient(
-        val ingredientName: String,
-        val ingredientValueType: String,
-        var ingredientCount: Int,
-    ) {
-
-    }
-    /////////
 
     fun addIngredient(ingredientName: String, ingredientValueType: String, ingredientCount: Int) {
         ingredientList.add(
@@ -64,11 +38,22 @@ class Recipe(
         }
     }
 
-    fun showRecipe() {
+    fun showRecipeIngredients() {
         for (i in ingredientList) {
             println("${i.ingredientName}: ${i.ingredientCount} ${i.ingredientValueType}")
         }
     }
+
+    fun showRecipeDescription() {
+        println(recipeDescription)
+    }
 }
 
 
+class Ingredient(
+    val ingredientName: String,
+    val ingredientValueType: String,
+    var ingredientCount: Int,
+) {
+
+}
