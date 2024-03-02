26f59d5e38c91922208c4187fb5cef9fed7de02c
Smokewolf <smokewolf@yandex.ru>
2024-03-02 21:51:45 +0300

    KS-13-1

---
+++ src/main/kotlin/lesson13/lesson13_task1.kt
@@ -0,0 +1,9 @@
+package lesson13
+
+class Phonebook(_name: String, _phonenumber: Long, _brand: String?){
+    val name:String = _name
+    val phoneNumber: Long = _phonenumber
+    val brand: String? = _brand
+}
+
+
