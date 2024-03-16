9840766f205a691f033643a851a75b079cc743c1
Smokewolf <smokewolf@yandex.ru>
2024-03-16 20:20:06 +0300

    KS-10-5

--- src/main/kotlin/lesson10/lesson10_task5.kt
+++ src/main/kotlin/lesson10/lesson10_task5.kt
@@ -4,33 +4,40 @@ fun main() {
     val userLogin = "Peter"
     val userPassword = "Password"
     val userShopList = listOf("Candy", "Bread", "Milk")
-    printShopList(userLogin, userPassword, userShopList)
+    println("Введите логин: ")
+    val inputLogin = readln()
+    println("Введите пароль: ")
+    val inputPassword = readln()
+    println( returnAuthorisationResult(userLogin, userPassword, userShopList, inputLogin, inputPassword))
 }
 
-fun printShopList(userLogin: String, userPassword: String, userShopList: List<String>) {
-    println("Введите логин: ")
-    val login = readln()
-    println("Введите пароль: ")
-    val password = readln()
-    val token = verification(userLogin, userPassword, login, password)
-    println(
-        if (token != null) {
-            "Приветствую, ${userLogin}. Ваша корзина:\n${userShopList.joinToString(separator = ", ")}"
-        } else {
-            "Авторизация неудалась. Попробуйте еще раз."
-        }
-    )
+fun returnAuthorisationResult(
+    userLogin: String,
+    userPassword: String,
+    userShopList: List<String>,
+    inputLogin: String,
+    inputPassword: String
+): String {
+    return if (verification(userLogin, userPassword, inputLogin, inputPassword) != null) {
+        "Приветствую, ${userLogin}. Ваша корзина:\n${userShopList.joinToString(separator = ", ")}"
+    } else {
+        "Авторизация неудалась. Попробуйте еще раз."
+    }
 }
 
 fun verification(userLogin: String, userPassword: String, inputLogin: String, inputPassword: String): String? {
-    var token: String? = ""
-    if (userLogin.lowercase() == inputLogin.lowercase() && userPassword == inputPassword) {
+    val isVerified = userLogin.lowercase() == inputLogin.lowercase() && userPassword == inputPassword
+    return createToken(isVerified)
+}
+
+fun createToken(isVerified: Boolean): String? {
+    var token = ""
+    if (isVerified) {
         repeat(TOKEN_LENGTH) {
             token += CHARS.random()
         }
-    } else return null
-
-    return token
+    }
+    return token.ifEmpty { null }
 }
 
 const val CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
