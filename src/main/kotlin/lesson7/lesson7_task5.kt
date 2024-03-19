2ec35984267a52b4ab1c2b7ad4f214fc6b86221c
Smokewolf <smokewolf@yandex.ru>
2024-03-19 09:30:01 +0300

    KS-7-5

--- src/main/kotlin/lesson7/lesson7_task5.kt
+++ src/main/kotlin/lesson7/lesson7_task5.kt
@@ -2,49 +2,30 @@ package lesson7
 
 fun main() {
     print("Введите желаемую длинну пароля: ")
-    val passwordSize = passwordLengthProcess(readln()) - COUNT_OF_CHAR_TYPES
+    val passwordSize = passwordLengthProcess(readln())
     val lowerCaseCharRange = 'a'..'z'
     val upperCaseCharRange = 'A'..'Z'
     val digitRange = '0'..'9'
     val allChars = lowerCaseCharRange + upperCaseCharRange + digitRange
-    var password = "" + lowerCaseCharRange.random() + upperCaseCharRange.random() + digitRange.random()
-    println(password)
-
-    for (step in passwordSize downTo 1) {
-        password += allChars.random()
+    val password: MutableList<Char> = mutableListOf()
+    password.add(lowerCaseCharRange.random())
+    password.add(upperCaseCharRange.random())
+    password.add(digitRange.random())
+    for (step in password.size until passwordSize) {
+        password.add(allChars.random())
     }
+    println(password.shuffled().joinToString(separator = ""))
 
-    password = password.shuffle()
-    println(password)
 }
-
-
-fun String.shuffle(): String {
-    val string: MutableList<String> = mutableListOf()
-    for (char in this) {
-        string.add(char.toString())
-    }
-    return string.shuffled().joinToString("")
-}
-
 
 fun passwordLengthProcess(inputLength: String): Int {
-    val passwordLength = try {
-        when (inputLength.toInt()) {
-            in 0..5 -> {
-                println("Введена слишком маленькая длинна пароля.")
-                println("Установленна минимальная величина - 6")
-                6
-            }
-
-            else -> inputLength.toInt()
-        }
-    } catch (e: Exception) {
-        println("Вы не ввели длинну пароля.")
-        println("Установленна минимальная величина - 6")
-        6
+    return if (inputLength.toInt() < MIN_PASSWORD_LENGTH) {
+        println("Введена слишком маленькая длинна пароля.")
+        println("Установленна минимальная величина - $MIN_PASSWORD_LENGTH")
+        MIN_PASSWORD_LENGTH
+    } else {
+        inputLength.toInt()
     }
-    return passwordLength
 }
 
-const val COUNT_OF_CHAR_TYPES = 3
+const val MIN_PASSWORD_LENGTH = 6
