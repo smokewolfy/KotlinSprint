2dc38d1ce1a8afd9dd4653b5e3fc55066e3cbf7b
Smokewolf <smokewolf@yandex.ru>
2024-03-24 14:27:14 +0300

    KS-16-2

---
+++ src/main/kotlin/lesson16/lesson16_task2.kt
@@ -0,0 +1,18 @@
+package lesson16
+
+class Circle(private val radius: Float){
+    private val pi = 3.14f
+    fun calcCircumference(): Float{
+        return pi * radius * 2
+    }
+    fun calcAreaOfCircle(): Float{
+        return pi * (radius * radius) * 2
+    }
+
+}
+
+fun main() {
+    val circle = Circle(7.0f)
+    println("Длина окружности: ${circle.calcCircumference()}")
+    println("Площадь окружности: ${circle.calcAreaOfCircle()}")
+}
