90ede552768542d82904c5cacb6b3356e29854e9
Smokewolf <smokewolf@yandex.ru>
2024-03-24 20:16:56 +0300

    KS-16-5

---
+++ src/main/kotlin/lesson16/lesson16_task5.kt
@@ -0,0 +1,47 @@
+package lesson16
+
+class Hero(
+    private val name: String,
+    private var health: Int = 100,
+    private var hitPower: Int,
+) {
+    private var isAlive = true
+
+    fun getName(): String = name
+    fun getAliveStatus(): Boolean = isAlive
+    fun hit(): Int = (0..hitPower).random()
+
+    fun heal() {
+        if (health < 91) health += 10 else health = 100
+    }
+
+    fun getDamage(damageValue: Int) {
+        health -= damageValue
+        if (health <= 0) {
+            dying()
+        }
+    }
+
+    private fun dying() {
+        health = 0
+        hitPower = 0
+        isAlive = false
+        println("Wasted. $name is dead.")
+    }
+}
+
+fun main() {
+    val hero1 = Hero(name = "Shao Kahn", hitPower = 20)
+    val hero2 = Hero("Liu Kang", hitPower = 20)
+    println("Fight!")
+    while (hero2.getAliveStatus() && hero1.getAliveStatus()) {
+        hero1.getDamage(hero2.hit())
+        if (hero2.getAliveStatus()) {
+            hero2.getDamage(hero1.hit())
+        }
+        hero1.heal()
+        hero2.heal()
+    }
+    val winnerName = if (hero1.getAliveStatus()) hero1.getName() else hero2.getName()
+    println("$winnerName Wins.")
+}
