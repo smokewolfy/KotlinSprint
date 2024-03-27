d7b81f5798561fa3fdb92ebcdd10a89e8f8025b4
Smokewolf <smokewolf@yandex.ru>
2024-03-27 22:48:15 +0300

    KS-17-1

---
+++ src/main/kotlin/lesson17/lesson17_task1.kt
@@ -0,0 +1,11 @@
+package lesson17
+
+class Quiz(_question: String, _answer: String) {
+    var question: String = _question
+        get() = "Question - $field"
+    var answer: String = _answer
+        get() = "Your answer - $field"
+        set(value) {
+            field = value
+        }
+}
