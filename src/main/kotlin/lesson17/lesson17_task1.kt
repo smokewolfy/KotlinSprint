11034ce57afc30df0086e6b3e93761439653ae22
Smokewolf <smokewolf@yandex.ru>
2024-03-27 22:59:41 +0300

    KS-17-1

--- src/main/kotlin/lesson17/lesson17_task1.kt
+++ src/main/kotlin/lesson17/lesson17_task1.kt
@@ -3,6 +3,7 @@ package lesson17
 class Quiz(_question: String, _answer: String) {
     var question: String = _question
         get() = "Question - $field"
+
     var answer: String = _answer
         get() = "Your answer - $field"
         set(value) {
