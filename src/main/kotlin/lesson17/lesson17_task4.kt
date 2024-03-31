61728f3851588fa9a4a7acb27e06678cbdb6d84e
Smokewolf <smokewolf@yandex.ru>
2024-03-31 10:48:13 +0300

    KS-17-4

---
+++ src/main/kotlin/lesson17/lesson17_task4.kt
@@ -0,0 +1,20 @@
+package lesson17
+
+class ParcelTracker(private val parcelID: Int, _currentLocation: String, private val destinationPoint: String) {
+    var currentLocation = _currentLocation
+        set(value) {
+            field = value
+            println(
+                if (field == destinationPoint)
+                    "Посылка прибыла в пункт назначения: $destinationPoint"
+                else
+                    "Посылка прибыла в пункт: $field"
+            )
+        }
+}
+
+fun main() {
+    val parcelTracker = ParcelTracker(0, "Moscow", "Voronezh")
+    parcelTracker.currentLocation = "Lipeck"
+    parcelTracker.currentLocation = "Voronezh"
+}
