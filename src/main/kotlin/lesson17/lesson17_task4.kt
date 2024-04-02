fe9ee52d974d12f6b4572b3c279e5e4c71bee374
Smokewolf <smokewolf@yandex.ru>
2024-04-02 21:27:48 +0300

    KS-17-4

--- src/main/kotlin/lesson17/lesson17_task4.kt
+++ src/main/kotlin/lesson17/lesson17_task4.kt
@@ -3,22 +3,17 @@ package lesson17
 class ParcelTracker(
     private val parcelID: Int,
     _currentLocation: String,
-    private val destinationPoint: String,
 ) {
+    private var relocationCounter = 0
     var currentLocation = _currentLocation
         set(value) {
             field = value
-            println(
-                if (field == destinationPoint)
-                    "Посылка прибыла в пункт назначения: $destinationPoint"
-                else
-                    "Посылка прибыла в пункт: $field"
-            )
+            relocationCounter++
         }
 }
 
 fun main() {
-    val parcelTracker = ParcelTracker(0, "Moscow", "Voronezh")
+    val parcelTracker = ParcelTracker(0, "Moscow")
     parcelTracker.currentLocation = "Lipeck"
     parcelTracker.currentLocation = "Voronezh"
 }
