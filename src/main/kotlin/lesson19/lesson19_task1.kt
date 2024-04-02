c75c4e26d72582fdf1ab0a858a04d700b5820baa
Smokewolf <smokewolf@yandex.ru>
2024-04-02 22:12:43 +0300

    KS-19-1

---
+++ src/main/kotlin/lesson19/lesson19_task1.kt
@@ -0,0 +1,13 @@
+package lesson19
+
+enum class Fishes(val fishName: String){
+    GUPPY("Гуппи"),
+    ANGELFISH("Скалярия"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок"),
+}
+
+fun main() {
+    println("Вы можете добавить этих рыбок в аквариум: ${Fishes.entries.joinToString { it.fishName }}.")
+}
+
