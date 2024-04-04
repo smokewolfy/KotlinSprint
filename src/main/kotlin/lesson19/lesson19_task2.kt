ee3002dbc8d2a471c0161040bee290fff4474766
Smokewolf <smokewolf@yandex.ru>
2024-04-04 21:28:23 +0300

    KS-19-2

---
+++ src/main/kotlin/lesson19/lesson19_task2.kt
@@ -0,0 +1,36 @@
+package lesson19
+
+enum class ProductCategory {
+    CLOTHES {
+        override fun categoryName() = "Одежда"
+    },
+    STATIONERY {
+        override fun categoryName() = "Канцелярские товары"
+    },
+    OVER {
+        override fun categoryName() = "Разное"
+    };
+
+    abstract fun categoryName(): String
+}
+
+class Product(private val name: String, private val id: Int, private val categoryName: ProductCategory) {
+    override fun toString(): String {
+        return "Товар: $name\nID: $id\nКатегория: ${
+            when (categoryName) {
+                ProductCategory.CLOTHES -> ProductCategory.CLOTHES.categoryName()
+                ProductCategory.STATIONERY -> ProductCategory.STATIONERY.categoryName()
+                else -> ProductCategory.OVER.categoryName()
+            }
+        }\n"
+    }
+}
+
+fun main() {
+    val pan = Product("Ручка", 1, ProductCategory.STATIONERY)
+    println(pan.toString())
+    val dress = Product("Платье", 2, ProductCategory.CLOTHES)
+    println(dress.toString())
+    val dogFood = Product("Собачий корм", 3, ProductCategory.OVER)
+    println(dogFood.toString())
+}
