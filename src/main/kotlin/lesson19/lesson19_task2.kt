dac6d648d01c830df82e454b8c7943bd196ad96a
Smokewolf <smokewolf@yandex.ru>
2024-06-12 18:57:04 +0300

    KS-19-2

--- src/main/kotlin/lesson19/lesson19_task2.kt
+++ src/main/kotlin/lesson19/lesson19_task2.kt
@@ -1,26 +1,18 @@
 package lesson19
 
-enum class ProductCategory {
-    CLOTHES {
-        override fun categoryName() = "Одежда"
-    },
-    STATIONERY {
-        override fun categoryName() = "Канцелярские товары"
-    },
-    OVER {
-        override fun categoryName() = "Разное"
-    };
-
-    abstract fun categoryName(): String
+enum class ProductCategory(val categoryName: String) {
+    CLOTHES("Одежда"),
+    STATIONERY("Канцелярские товары"),
+    OTHER("Разное")
 }
 
 class Product(private val name: String, private val id: Int, private val categoryName: ProductCategory) {
     override fun toString(): String {
         return "Товар: $name\nID: $id\nКатегория: ${
             when (categoryName) {
-                ProductCategory.CLOTHES -> ProductCategory.CLOTHES.categoryName()
-                ProductCategory.STATIONERY -> ProductCategory.STATIONERY.categoryName()
-                else -> ProductCategory.OVER.categoryName()
+                ProductCategory.CLOTHES -> ProductCategory.CLOTHES.categoryName
+                ProductCategory.STATIONERY -> ProductCategory.STATIONERY.categoryName
+                else -> ProductCategory.OTHER.categoryName
             }
         }\n"
     }
@@ -31,6 +23,6 @@ fun main() {
     println(pan.toString())
     val dress = Product("Платье", 2, ProductCategory.CLOTHES)
     println(dress.toString())
-    val dogFood = Product("Собачий корм", 3, ProductCategory.OVER)
+    val dogFood = Product("Собачий корм", 3, ProductCategory.OTHER)
     println(dogFood.toString())
 }
