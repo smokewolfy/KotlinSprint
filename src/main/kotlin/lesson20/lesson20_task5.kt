5d6bbc95f84802e7780dbd517dfa16e95b7d93b5
smokewolfy <72520374+smokewolfy@users.noreply.github.com>
2024-06-15 12:31:25 +0300

    Update lesson20_task5.kt

--- src/main/kotlin/lesson20/lesson20_task5.kt
+++ src/main/kotlin/lesson20/lesson20_task5.kt
@@ -4,14 +4,11 @@ class Robot() {
     private val speechList = listOf("I am robot.", "Yes, I do.", "Yes, I can.", "No, I don't.", "No, I can't.")
     fun say(isReversed: Boolean) {
         println(modifier(speechList.random(), isReversed))
-
     }
 
     private val modifier: (String, Boolean) -> String =
         { speech: String, isReversed: Boolean -> if (isReversed) speech.reversed() else speech }
-
 }
-
 
 fun main() {
     val robot = Robot()
